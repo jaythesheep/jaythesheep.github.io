effd7020fac0db664bb083d1a34a853747d3dcff
YannickSchuetze <yannick.schuetze@gmx.de>
2020-05-13 11:48:05 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,61 +1,112 @@
 $(document).foundation();
 
-$('.top-bar').on('sticky.zf.stuckto:top', function(){
-    $(this).addClass('shrink');
-  }).on('sticky.zf.unstuckfrom:top', function(){
-    $(this).removeClass('shrink');
-  });
+$('.top-bar').on('sticky.zf.stuckto:top', function () {
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function () {
+  $(this).removeClass('shrink');
+});
 
-  $(document).ready(function() {
-    var time = 5;
-    var $bar,
-        $slick,
-        isPause,
-        tick,
-        percentTime;
 
-    $slick = $('.slider');
-    $slick.slick({
-      draggable: false,
-      adaptiveHeight: false,
-      dots: false,
-      arrows: false
+
+
+
+// Cache selectors
+var lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight()+165,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
     });
 
-    $bar = $('.slider-progress .progress');
-  
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
 
-  
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+
+
+
+
+
+$(document).ready(function () {
+  var time = 5;
+  var $bar,
+    $slick,
+    isPause,
+    tick,
+    percentTime;
+
+  $slick = $('.slider');
+  $slick.slick({
+    draggable: false,
+    adaptiveHeight: false,
+    dots: false,
+    arrows: false
+  });
+
+  $bar = $('.slider-progress .progress');
+
+
+
   function startProgressbar() {
     resetProgressbar();
     percentTime = 0;
     isPause = false;
     tick = setInterval(interval, 10);
   }
-  
+
   function interval() {
-    if(isPause === false) {
-      percentTime += 1 / (time+0.1);
+    if (isPause === false) {
+      percentTime += 1 / (time + 0.1);
       $bar.css({
-        width: percentTime+"%"
+        width: percentTime + "%"
       });
-      if(percentTime >= 100)
-        {
-          $slick.slick('slickNext');
-          startProgressbar();
-        }
+      if (percentTime >= 100) {
+        $slick.slick('slickNext');
+        startProgressbar();
+      }
     }
   }
-  
-  
+
+
   function resetProgressbar() {
     $bar.css({
-     width: 0+'%' 
+      width: 0 + '%'
     });
     clearTimeout(tick);
   }
-  
-  startProgressbar();
-  })
 
-  
+  startProgressbar();
+})
